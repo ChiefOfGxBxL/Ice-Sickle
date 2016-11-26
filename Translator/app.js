ee8e59057b1234e24e23e30eb1609b3daf344fb9
ChiefOfGxBxL <chiefofgxbxl@github.com>
2016-11-26 15:17:48 -0500

    Units translator added with sample data to place units on the map.

--- Translator/app.js
+++ Translator/app.js
@@ -1,19 +1,22 @@
 var fs = require('fs'),
-    doodads = JSON.parse(fs.readFileSync('L:\\Users\\Greg\\Desktop\\doodads.json', 'utf8')),
-    terrain = JSON.parse(fs.readFileSync('L:\\Users\\Greg\\Desktop\\terrain.json', 'utf8')),
-    strings = JSON.parse(fs.readFileSync('L:\\Users\\Greg\\Desktop\\strings.json', 'utf8')),
+    units = JSON.parse(fs.readFileSync('sample data\\units.json', 'utf8')),
+    doodads = JSON.parse(fs.readFileSync('sample data\\doodads.json', 'utf8')),
+    terrain = JSON.parse(fs.readFileSync('sample data\\terrain.json', 'utf8')),
+    strings = JSON.parse(fs.readFileSync('sample data\\strings.json', 'utf8')),
     
     // translators
+    UnitsTranslator = require('./translators/UnitsTranslator'),
     DoodadsTranslator = require('./translators/DoodadsTranslator'),
     TerrainTranslator = require('./translators/TerrainTranslator'),
     StringsTranslator = require('./translators/StringsTranslator'),
     
     // instances of translators
+    unitsTranslator = new UnitsTranslator('war3mapUnits.doo', units),
     doodadsTranslator = new DoodadsTranslator('war3map.doo', doodads),
     terrainTranslator = new TerrainTranslator('war3map.w3e', terrain),
     stringsTranslator = new StringsTranslator('war3map.wts', strings);
     
+unitsTranslator.write();
 doodadsTranslator.write();
 terrainTranslator.write();
 stringsTranslator.write();
-
