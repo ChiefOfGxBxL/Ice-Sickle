3872b3accaf37686ee6e8527a5cdb981a261ee9e
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-05-05 12:02:58 -0400

    Create Settings class to store editor settings
    Closes #1

---
+++ classes/Settings.js
@@ -0,0 +1,25 @@
+var fs = require('fs-extra'),
+    Path = require('path'),
+    currPath; // Stores the path to the settings.json file
+
+var Settings = {
+    Save: function() {
+        fs.ensureFileSync(currPath);
+        fs.writeJsonSync(currPath, Settings);
+    },
+    
+    Load: function(dir) {
+        // Load the settings into memory
+        currPath = Path.resolve(dir, 'settings.json');
+        var data = fs.readJsonSync(currPath);
+
+        // Add them to the Settings object
+        Object.keys(data).forEach((key) => {
+            if(key.toLowerCase() !== 'save' && key.toLowerCase() !== 'load') {
+                Settings[key] = data[key];
+            }
+        });
+    }
+}
+
+module.exports = Settings;
