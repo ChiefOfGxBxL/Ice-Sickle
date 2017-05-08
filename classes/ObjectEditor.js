25012bf78bec936a94acff5efcfb49987c0f6ef6
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-05-08 18:19:51 -0400

    Create new ObjectEditor "class" to store all core data for WE

---
+++ classes/ObjectEditor.js
@@ -0,0 +1,12 @@
+
+
+var ObjectEditor = {
+    Data: {
+        Units: require('../WEdata/data/Units.json')
+    },
+    Fields: {
+        Units: require('../WEdata/fields/UnitMetaData.json')
+    }
+}
+
+module.exports = ObjectEditor;
