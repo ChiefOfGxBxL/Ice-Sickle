f7584ce6d00b62cdc6187c296fd1027a0a735d66
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-05-08 18:19:05 -0400

    Create two-col-sel "library" for listing a <select> list with two data columns (like the WE object editor)

---
+++ assets/js/two-col-sel.js
@@ -0,0 +1,16 @@
+function spaceSelect(selElem, size) {
+  selElem.classList.add('two-col-sel');
+
+  var childOptions = selElem.querySelectorAll('option');
+
+  for(var i = 0; i < childOptions.length; i++) {
+    var parts = childOptions[i].innerText.split('|'),
+		spacesToAdd = (size || 60) - parts[0].trim().length - parts[1].trim().length,
+        repeatSpace = function(num) {
+        	return Array(num + 1).join('\u00a0');
+        };
+
+    // Set the text of the option to contain the padding spaces
+    childOptions[i].innerText = parts[0] + repeatSpace(spacesToAdd) + parts[1] + repeatSpace(2);
+  }
+}
