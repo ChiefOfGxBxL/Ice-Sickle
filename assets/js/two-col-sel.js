3f7b66320f13c7db3f34432851124291c5ec2036
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-05-08 22:16:13 -0400

    Add check if unable to split | character in two-col-sel

--- assets/js/two-col-sel.js
+++ assets/js/two-col-sel.js
@@ -4,13 +4,19 @@ function spaceSelect(selElem, size) {
   var childOptions = selElem.querySelectorAll('option');
 
   for(var i = 0; i < childOptions.length; i++) {
-    var parts = childOptions[i].innerText.split('|'),
-		spacesToAdd = (size || 60) - parts[0].trim().length - parts[1].trim().length,
-        repeatSpace = function(num) {
-        	return Array(num + 1).join('\u00a0');
-        };
+    var parts = childOptions[i].innerText.split('|');
 
-    // Set the text of the option to contain the padding spaces
-    childOptions[i].innerText = parts[0] + repeatSpace(spacesToAdd) + parts[1] + repeatSpace(2);
+    if(parts.length == 2) {
+        var spacesToAdd = (size || 60) - parts[0].trim().length - parts[1].trim().length,
+            repeatSpace = function(num) {
+            	return Array(num + 1).join('\u00a0');
+            };
+
+        // Set the text of the option to contain the padding spaces
+        childOptions[i].innerText = parts[0] + repeatSpace(spacesToAdd) + parts[1] + repeatSpace(2);
+    }
+    else {
+        // Otherwise we keep the childOptions innerText the same
+    }
   }
 }
