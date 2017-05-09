827a40a034ed24a8b2c7659855902b4cb3be8fe5
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-05-08 22:17:24 -0400

    Add Find subsection to ObjectEditor for utility functions for traversing the objects

--- classes/ObjectEditor.js
+++ classes/ObjectEditor.js
@@ -6,6 +6,23 @@ var ObjectEditor = {
     },
     Fields: {
         Units: require('../WEdata/fields/UnitMetaData.json')
+    },
+
+    Find: {
+        UnitFieldIdByLongName: function(longName) {
+            // e.g. bountydice -> ubdi
+            // TODO: time this function -- it may take too long for lookups
+
+            for(var fieldKey in ObjectEditor.Fields.Units) {
+                if( ObjectEditor.Fields.Units[fieldKey] &&
+                    ObjectEditor.Fields.Units[fieldKey].field.toLowerCase() === longName.toLowerCase()) {
+                    return fieldKey;
+                }
+            }
+
+            // console.error('Unable to lookup unit field by long name:', longName);
+            return null; // Unable to find by long name
+        }
     }
 }
 
