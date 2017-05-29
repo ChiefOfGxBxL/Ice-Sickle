48abbdd49c5119029644b98dcf695e927d5d3e69
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-05-29 12:21:37 -0400

    Create ObjectEntityType enum for referencing the different types of object entities

---
+++ enum/ObjectEntityType.js
@@ -0,0 +1,10 @@
+module.exports = {
+    Unit:           'unit',
+    Item:           'item',
+    Destructable:   'destructable',
+    Doodad:         'doodad',
+    Ability:        'ability',
+    Buff:           'buff',
+    Effect:         'buff', // This is here for safe-keeping; same as Buff
+    Upgrade:        'upgrade'
+}
