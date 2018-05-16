7b347e3d2a29f32c6f67cd0730b5339c8b57f049
ChiefOfGxBxL <chiefofgxbxl@github.com>
2018-05-15 20:11:16 -0400

    Send manifest through cb in Uninstall method

--- classes/PluginManager.js
+++ classes/PluginManager.js
@@ -73,12 +73,14 @@ var PluginManager = {
 
         // Unregister event listeners :: TODO
 
-        // Remove from list of loaded plugins
-        delete PluginManager.loadedPlugins[name];
-
         // Delete extension from file system
         fs.remove(extensionDirPath, function(err, result) {
-            return cb(err, null);
+            const extensionManifest = PluginManager.loadedPlugins[name].manifest;
+
+            // Remove from list of loaded plugins
+            delete PluginManager.loadedPlugins[name];
+
+            return cb(err, extensionManifest);
         });
     }
 }
