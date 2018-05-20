a584c17a12e922056d4d457b32aea68da67511b5
ChiefOfGxBxL <chiefofgxbxl@github.com>
2018-05-19 20:52:16 -0400

    Develop ExtensionAPI which gets passed in to each Extension instance

--- classes/ExtensionAPI.ts
+++ classes/ExtensionAPI.ts
@@ -1,7 +1,12 @@
 import { Logger } from './Logger';
+import { Settings } from './Settings';
+import { EventMgr, EventListener } from './EventMgr';
+import { EventType } from './EventType';
 
-export class ExtensionAPI {
-
-    Logger: Logger
-
+export abstract class ExtensionAPI {
+    static Logger = Logger
+    static Settings = Settings
+    static EventMgr = EventMgr
+    static EventListener = EventListener
+    static EventType = EventType
 }
